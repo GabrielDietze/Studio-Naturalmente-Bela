bb23d543ac946e218856dfdee4e204b7d5aeff24
Gabriel Augusto Dietze Novy <gabriel.novy@ferroeste.com.br>
2026-02-10 08:48:06 -0300

    correção performance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/lato/300.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+import '@fontsource/cormorant-garamond/300.css';
+import '@fontsource/cormorant-garamond/400.css';
+import '@fontsource/cormorant-garamond/500.css';
+import '@fontsource/cormorant-garamond/600.css';
 import './index.css';
 import App from './App';
 
